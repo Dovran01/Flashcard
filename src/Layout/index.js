03e09975b2cadd095cc8cc8c83a299b775e5bc23
dovran_masharipov <120339031+Dovran01@users.noreply.github.com>
2023-10-03 15:08:15 -0400

    changes

---
+++ src/Layout/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import DeckList from "../components/deck/DeckList";
+import CreateNewDeck from "../components/deck/CreateNewDeck";
+import Study from "../components/study/Study";
+import CreateNewCard from "../components/card/CreateNewCard";
+import Header from "./Header";
+import NotFound from "./NotFound";
+import ViewDeck from "../components/deck/ViewDeck";
+import EditCard from "../components/card/EditCard";
+import EditDeck from "../components/deck/EditDeck";
+
+function Layout() {
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <DeckList />
+          </Route>
+          <Route path="/decks/new">
+            <CreateNewDeck />
+          </Route>
+          <Route exact path="/decks/:deckId">
+            <ViewDeck />
+          </Route>
+          <Route path="/decks/:deckId/study">
+            <Study />
+          </Route>
+          <Route path="/decks/:deckId/cards/new">
+            <CreateNewCard />
+          </Route>
+          <Route path="/decks/:deckId/cards/:cardId/edit">
+            <EditCard />
+          </Route>
+          <Route path="/decks/:deckId/edit">
+            <EditDeck />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Layout;
